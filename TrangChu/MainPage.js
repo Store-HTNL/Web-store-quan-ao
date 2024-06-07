fd9b88d7bb8e51dcab2bbb8c2c56bf4159fade62
TT23520 <thinh2352006@gmail.com>
2024-06-07 20:49:43 +0700

    store quan ao

---
+++ TrangChu/MainPage.js
@@ -0,0 +1,93 @@
+function hienThiDanhSachSanPhamAo() {
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let danhSachSanPham = document.getElementById("danhsachsanphamao");
+    danhSachSanPham.innerHTML = '';
+    let row = document.createElement('div');
+    row.classList.add('product-row');
+    
+    let sanPhamAo = luutrusanpham.filter(sanPham => sanPham.sanpham === "áo");
+    for (let i = 0; i < sanPhamAo.length && i < 3; i++) {
+        let sanpham = sanPhamAo[i];
+        let productCell = document.createElement('div');
+        let productCell1 = document.createElement('div');
+        let productCell2 = document.createElement('div');
+        productCell.classList.add('product-cell');
+        productCell2.classList.add('product-cell2');
+
+        let hinhSanPham = document.createElement('img');
+        hinhSanPham.classList.add('pic1');
+        hinhSanPham.src = sanpham.urlhinhanh;
+        let tenSanPham = document.createElement('h3');
+        tenSanPham.classList.add('name');
+        tenSanPham.textContent = sanpham.tensanpham;
+        let giaSanPham = document.createElement('p');
+        giaSanPham.classList.add('price');
+        giaSanPham.textContent = sanpham.gia;
+         productButton = document.createElement('a');
+        productButton.classList.add('chitiet-btn');
+        productButton.textContent = 'Chi tiết';
+        productButton.onclick = () =>{
+            localStorage.setItem('sanPhamChiTiet', JSON.stringify(sanpham));
+            window.location.href = '/Đồ Án/TrangChu/chitietsanpham.html';
+        }
+
+        productCell.appendChild(hinhSanPham);
+        productCell1.appendChild(tenSanPham);
+        productCell1.appendChild(giaSanPham);
+        productCell1.appendChild(productButton);
+
+
+        productCell2.appendChild(productCell);
+        productCell2.appendChild(productCell1);
+        row.appendChild(productCell2);
+    }
+    danhSachSanPham.appendChild(row);
+}
+function hienThiDanhSachSanPhamQuan() {
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let danhSachSanPham = document.getElementById("danhsachsanphamquan");
+    danhSachSanPham.innerHTML = '';
+    let sanPhamQuan = luutrusanpham.filter(sanPham => sanPham.sanpham === "quần");
+    let row = document.createElement('div');
+    row.classList.add('product-row');
+
+    for (let i = 0; i < sanPhamQuan.length && i < 3; i++) {
+        let sanpham = sanPhamQuan[i];
+        let productCell = document.createElement('div');
+        let productCell1 = document.createElement('div');
+        let productCell2 = document.createElement('div');
+        productCell.classList.add('product-cell');
+        productCell2.classList.add('product-cell2');
+        
+        let hinhSanPham = document.createElement('img');
+        hinhSanPham.classList.add('pic1');
+        hinhSanPham.src = sanpham.urlhinhanh;
+        let tenSanPham = document.createElement('h3');
+        tenSanPham.classList.add('name');
+        tenSanPham.textContent = sanpham.tensanpham;
+        let giaSanPham = document.createElement('p');
+        giaSanPham.classList.add('price');
+        giaSanPham.textContent = sanpham.gia;
+        let productButton = document.createElement('a');
+        productButton.classList.add('chitiet-btn');
+        productButton.textContent = 'Chi tiết';
+        productButton.onclick = () =>{
+            localStorage.setItem('sanPhamChiTiet', JSON.stringify(sanpham));
+            window.location.href = '/Đồ Án/TrangChu/chitietsanpham.html';
+        }
+        
+        productCell.appendChild(hinhSanPham);
+        productCell1.appendChild(tenSanPham);
+        productCell1.appendChild(giaSanPham);
+        productCell1.appendChild(productButton);
+
+        productCell2.appendChild(productCell);
+        productCell2.appendChild(productCell1);
+        row.appendChild(productCell2);
+    }
+    danhSachSanPham.appendChild(row);
+}
+window.onload = function(){
+    hienThiDanhSachSanPhamAo()
+    hienThiDanhSachSanPhamQuan()
+};
---
+++ themsanpham/themsanpham.js
@@ -0,0 +1,88 @@
+function themsanpham() {
+    let masanpham = document.getElementById('masanpham').value;
+    let tensanpham = document.getElementById('tensanpham').value;
+    let sanpham = document.getElementById('sanpham').value;
+    let phanloai = document.getElementById('phanloai').value;
+    let urlhinhanh = document.getElementById('urlhinhanh').value;
+    let mota = document.getElementById('mota').value;
+    let thuonghieuSP = document.getElementById('thuonghieuSP').value;
+    let SLtonkho = document.getElementById('SLtonkho').value;
+    let SLthucte = document.getElementById('SLthucte').value;
+    let gia = document.getElementById('gia').value;
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let sanphamdaco = luutrusanpham.find(luutrusanpham => luutrusanpham.tensanpham === tensanpham);
+
+    if (sanphamdaco) {
+        document.getElementById("displayData").textContent = "Sản phẩm không hợp lệ hoặc đã tồn tại.";
+        return;
+    }
+
+    luutrusanpham.push({ masanpham, tensanpham, sanpham, phanloai, urlhinhanh, mota, thuonghieuSP, SLtonkho, SLthucte, gia});
+
+    localStorage.setItem('luutrusanpham', JSON.stringify(luutrusanpham));
+    nutchuensanpham();
+    updatesanpham();
+    alert('Thêm thành công!');
+}
+window.onload = function() {
+    updatesanpham();
+    nutchuensanpham();
+}
+function updatesanpham(){
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let danhSachSanPham = document.getElementById('danhSachSanPham');
+    danhSachSanPham.innerHTML = '';
+
+    luutrusanpham.forEach((sanpham) => {
+        let listItem = document.createElement('li');
+        listItem.textContent = sanpham.masanpham + ' ' + sanpham.tensanpham;
+        danhSachSanPham.appendChild(listItem);
+    });
+}
+function xoasanpham(){
+    let macanxoa = document.getElementById('macanxoa').value
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let sanphamcanxoa = luutrusanpham.findIndex(luutrusanpham => luutrusanpham.masanpham === macanxoa);
+
+    if (sanphamcanxoa !== -1) {
+        luutrusanpham.splice(sanphamcanxoa, 1);
+        localStorage.setItem('luutrusanpham', JSON.stringify(luutrusanpham));
+        updatesanpham()
+        alert('Xoá sản phẩm thành công')
+        return;
+    }
+    alert('Không tìm thấy sản phẩm!');
+}
+function nutchuensanpham(){
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let nutchuyensanpham = document.getElementById("chuyensanpham");
+
+    luutrusanpham.forEach((sanpham, index) => {
+    let button = document.createElement("button");
+    button.textContent = sanpham.tensanpham;
+    button.addEventListener("click", () => taisanpham(index));
+    nutchuyensanpham.appendChild(button);
+    });
+}
+function taisanpham(index) {
+    let luutrusanpham = JSON.parse(localStorage.getItem('luutrusanpham')) || [];
+    let sanpham = luutrusanpham[index];
+    document.getElementById("ten").textContent = sanpham.tensanpham;
+    document.getElementById("gioithieu").textContent = sanpham.mota;
+    document.getElementById("giatien").textContent = "Giá: " + sanpham.gia;
+    document.getElementById("hinh").src = sanpham.urlhinhanh;
+}
+
+function chitietsanphambt(){
+    taisanpham(0);
+    let sanpham = document.getElementById('sanphamscreen');
+    let quanly = document.getElementById('quanly');
+    quanly.style.display = 'none';
+    sanpham.style.display = 'block';
+}
+function quanlysanphambt(){
+    let sanpham = document.getElementById('sanphamscreen');
+    let quanly = document.getElementById('quanly');
+    quanly.style.display = 'block';
+    sanpham.style.display = 'none';
+}
